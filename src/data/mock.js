8ddf9e1ce59610abb03b30cb74682bba8fa199bd
unknown <ieiayaobb@qq.com>
2017-07-24 23:15:16 +0800

    change structure of mock data

--- src/data/mock.js
+++ src/data/mock.js
@@ -1,116 +1,142 @@
-module.exports = [{
-    "id": 1,
-    "player_a": {
-      "id": 1,
-      "img": "",
-      "name": "player1",
-      "score": 2
-    },
-    "player_b": {
-      "id": 2,
-      "img": "",
-      "name": "player2",
-      "score": 1
-    },
-    "pid": 12
-  },
-  {
-    "id": 2,
-    "player_a": {
-      "id": 3,
-      "img": "",
-      "name": "player3",
-      "score": 2
-    },
-    "player_b": {
-      "id": 4,
-      "img": "",
-      "name": "player4",
-      "score": 1
-    },
-    "pid": 12
-  },
-  {
-    "id": 3,
-    "player_a": {
+module.exports = [
+  [{
       "id": 5,
-      "img": "",
-      "name": "player5",
-      "score": 2
+      "player_a": {
+        "id": 1,
+        "img": "",
+        "name": "player1",
+        "score": 2
+      },
+      "player_b": {
+        "id": 9,
+        "img": "",
+        "name": "player9",
+        "score": 3
+      },
+      "pid": 4
     },
-    "player_b": {
+    {},
+    {},
+    {},
+    {
       "id": 6,
-      "img": "",
-      "name": "player6",
-      "score": 1
+      "player_a": {
+        "id": 5,
+        "img": "",
+        "name": "player5",
+        "score": 2
+      },
+      "player_b": {
+        "id": 10,
+        "img": "",
+        "name": "player10",
+        "score": 3
+      },
+      "pid": 3
     },
-    "pid": 34
-  },
-  {
-    "id": 4,
-    "player_a": {
-      "id": 7,
-      "img": "",
-      "name": "player7",
-      "score": 2
-    },
-    "player_b": {
-      "id": 8,
-      "img": "",
-      "name": "player8",
-      "score": 1
-    },
-    "pid": 34
-  },
-  {
-    "id": 5,
-    "player_a": {
-      "id": 7,
-      "img": "",
-      "name": "player7",
-      "score": 2
-    },
-    "player_b": {
-      "id": 9,
-      "img": "",
-      "name": "player8",
-      "score": 1
-    },
-    "pid": 4
-  },
-  {
-    "id": 12,
-    "player_a": {
+    {},
+    {},
+    {},
+  ],
+  [{
       "id": 1,
-      "img": "",
-      "name": "player1",
-      "score": 2
+      "player_a": {
+        "id": 1,
+        "img": "",
+        "name": "player1",
+        "score": 2
+      },
+      "player_b": {
+        "id": 2,
+        "img": "",
+        "name": "player2",
+        "score": 1
+      },
+      "pid": 12
     },
-    "player_b": {
+    {
+      "id": 2,
+      "player_a": {
+        "id": 3,
+        "img": "",
+        "name": "player3",
+        "score": 2
+      },
+      "player_b": {
+        "id": 4,
+        "img": "",
+        "name": "player4",
+        "score": 1
+      },
+      "pid": 12
+    },
+    {
       "id": 3,
-      "img": "",
-      "name": "player3",
-      "score": 1
+      "player_a": {
+        "id": 5,
+        "img": "",
+        "name": "player5",
+        "score": 2
+      },
+      "player_b": {
+        "id": 6,
+        "img": "",
+        "name": "player6",
+        "score": 1
+      },
+      "pid": 34
     },
-    "pid": 1234
-  },
-  {
-    "id": 34,
-    "player_a": {
-      "id": 6,
-      "img": "",
-      "name": "player6",
-      "score": 2
+    {
+      "id": 4,
+      "player_a": {
+        "id": 7,
+        "img": "",
+        "name": "player7",
+        "score": 2
+      },
+      "player_b": {
+        "id": 8,
+        "img": "",
+        "name": "player8",
+        "score": 1
+      },
+      "pid": 34
+    }
+  ],
+  [{
+      "id": 12,
+      "player_a": {
+        "id": 1,
+        "img": "",
+        "name": "player1",
+        "score": 2
+      },
+      "player_b": {
+        "id": 3,
+        "img": "",
+        "name": "player3",
+        "score": 1
+      },
+      "pid": 1234
     },
-    "player_b": {
-      "id": 7,
-      "img": "",
-      "name": "player7",
-      "score": 1
-    },
-    "pid": 1234
-  },
-  {
+    {
+      "id": 34,
+      "player_a": {
+        "id": 6,
+        "img": "",
+        "name": "player6",
+        "score": 2
+      },
+      "player_b": {
+        "id": 7,
+        "img": "",
+        "name": "player7",
+        "score": 1
+      },
+      "pid": 1234
+    }
+  ],
+  [{
     "id": 1234,
     "player_a": {
       "id": 1,
@@ -125,5 +151,5 @@ module.exports = [{
       "score": 1
     },
     "pid": -1
-  }
+  }]
 ]
--- src/vuex/modules/init-data.js
+++ src/vuex/modules/init-data.js
@@ -17,45 +17,68 @@ const getters = {
 
 const mutations = {
 	[INIT_BRACKET] (state, response) {
-		var data = response.messages
-		
-		var root;
-		var rounds = []
-		_.forEach(data, function (match) {
-			if (match.pid == '-1') {
-				root = match
-			}
+		state.initData = response.messages
+		var firstRound = response.messages[0]
+		var firstFilterdRound = _.reject(firstRound, function(e) {
+			return _.isEmpty(e)
 		})
 
-		rounds.push([root])
-
-		var level = 0
-		var buildRoot = function (beginNode) {
-			level ++
-			_.forEach(data, function (node) {
-				if (node.pid == beginNode.id) {
-					if (!rounds[level]) {
-						rounds[level] = []
-					}
-					rounds[level].push(node)
-
-					buildRoot(node)
-				}
-			})
-			level --
-		}
-		buildRoot(root)
-		
-		var rounds_length = rounds.length
-		var possibleCount = rounds[rounds.length - 1].length
-		if (possibleCount * 2 == Math.pow(2, rounds_length)) {
-			state.totalCount = possibleCount * 2
+		if (firstFilterdRound.length == firstRound.length) {
+			state.totalCount = firstFilterdRound.length * 2
 		} else {
-			state.totalCount = possibleCount + rounds[rounds.length - 2].length * 2
+			state.totalCount = response.messages[1].length * 2 + firstFilterdRound.length
 		}
 
-		state.initData = rounds.reverse()
-	},
+	}
+	// [INIT_BRACKET] (state, response) {
+	// 	var data = response.messages
+		
+	// 	var root;
+	// 	var rounds = []
+	// 	_.forEach(data, function (match) {
+	// 		if (match.pid == '-1') {
+	// 			root = match
+	// 		}
+	// 	})
+
+	// 	rounds.push([root])
+
+	// 	var level = 0
+	// 	var buildRoot = function (beginNode) {
+	// 		level ++
+	// 		_.forEach(data, function (node) {
+	// 			if (node.pid == beginNode.id) {
+	// 				if (!rounds[level]) {
+	// 					rounds[level] = new Array(Math.pow(2, level))
+	// 				}
+	// 				(function(node, level){
+	// 					_.forEach(rounds[level], function(ele, i) {
+	// 						console.log(level, i)
+	// 						if(!ele) {
+	// 							rounds[level][i] = node
+	// 							return false
+	// 						}
+	// 					})
+	// 				}(node, level))
+
+	// 				buildRoot(node)
+	// 			}
+	// 		})
+	// 		level --
+	// 	}
+	// 	buildRoot(root)
+
+	// 	var rounds_length = rounds.length
+	// 	var possibleCount = rounds[rounds.length - 1].length
+	// 	state.totalCount = possibleCount * 2
+	// 	// if (possibleCount * 2 == Math.pow(2, rounds_length)) {
+	// 	// 	state.totalCount = possibleCount * 2
+	// 	// } else {
+	// 	// 	state.totalCount = possibleCount + rounds[rounds.length - 2].length * 2
+	// 	// }
+
+	// 	state.initData = rounds.reverse()
+	// },
 }
 
 export default {
